9355757b8d0cfcadab0a04d6563c43b9c0b74804
sunrit-noob <sunrit2022pers@gmail.com>
2025-03-19 13:50:40 +0530

    Changes

--- arrange.py
+++ arrange.py
@@ -5,15 +5,15 @@ import argparse
 def arrange_files(args):
     host_dir = args.host_dir
     target_dir = args.target_dir
-    if not os.path.exists(target_dir):
-        os.makedirs(target_dir)
     classes = os.listdir(host_dir)
     for class_name in classes:
+        if not os.path.exists(os.path.join(target_dir, class_name)):
+            os.makedirs(os.path.join(target_dir, class_name))
         instances = os.listdir(os.path.join(host_dir, class_name))
         for instance_name in instances:
             image = Image.open(os.path.join(host_dir, class_name, instance_name, 'rendering/00.png'))
             image_resized = image.resize((256, 256))
-            image_resized.save(os.path.join(target_dir, f'{class_name}_{instance_name}.png'))
+            image_resized.save(os.path.join(target_dir, class_name, f'{class_name}_{instance_name}.png'))
         print(f'--------{class_name} Arranged--------')
 
 def main():
